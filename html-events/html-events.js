4f032088c37a98890c6cdcc824bfedb4716e5efd
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:30:04 +0300

    1.5

--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,7 @@
+function colors(){
+	var color = prompt("what color do u want to use?");
+    var background = document.getElementsByTagName("body");
+    background[0].style.backgroundColor = color;
+
+}
 // Put all your JavaScript in this file!
--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,17 @@
+var me = {
+	 firstname = "adoo"
+	 lastname = "faddoul"
+	 favoritefood = "seafood"
+	 age = 16
+	 picture = 
+	}
+	
+
+
+
+
+
+
+
+
 // Create your 'me' object literal here!
